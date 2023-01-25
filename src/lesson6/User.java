326a06617431ba1ebcd4d38735b35b2944ba34b0
Ruslans Tufetulovs <ruslans.tufetulovs@meawallet.com>
2023-01-25 11:31:09 +0200

    lesson 6

---
+++ src/lesson6/User.java
@@ -0,0 +1,36 @@
+package lesson6;
+
+import java.util.Objects;
+
+public class User {
+
+    public String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(name, user.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+}
