07f1577506f724bbbf98e21490ce8df9ad5dcec0
Ruslans Tufetulovs <ruslans.tufetulovs@meawallet.com>
2023-02-08 11:43:08 +0200

    lesson 10

---
+++ src/lesson10/libraryapp/author/AuthorDto.java
@@ -0,0 +1,25 @@
+package lesson10.libraryapp.author;
+
+public class AuthorDto {
+
+    private String name;
+    private String lastname;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+
+}
---
+++ src/lesson10/libraryapp/author/AuthorDtoConverter.java
@@ -0,0 +1,11 @@
+package lesson10.libraryapp.author;
+
+public class AuthorDtoConverter {
+
+    public AuthorDto convert(Author author) {
+        AuthorDto dto = new AuthorDto();
+        dto.setName(author.getName());
+        dto.setLastname(author.getLastname());
+        return dto;
+    }
+}
